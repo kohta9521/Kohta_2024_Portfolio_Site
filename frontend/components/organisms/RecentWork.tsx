a426f2095d45f74b9fe146665fdb39a204679294
k_95.21 <kohta0421kochi@gmail.com>
2024-04-03 12:54:06 +0900

    add recent area parent components

---
+++ frontend/components/organisms/RecentWork.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// scss
+import styles from "./styles/RecentWork.module.scss";
+
+// component
+
+const RecentWork = () => {
+  return (
+    <div className={styles.recent}>
+      <div className={styles.container}>
+        <h1>recent works area</h1>
+      </div>
+    </div>
+  );
+};
+
+export default RecentWork;
