40e9a2514aeae16f3aca64c8eee383a6b807f1b5
k_95.21 <kohta0421kochi@gmail.com>
2024-04-03 13:48:42 +0900

    add new recent work card

--- frontend/components/organisms/RecentWork.tsx
+++ frontend/components/organisms/RecentWork.tsx
@@ -5,12 +5,23 @@ import styles from "./styles/RecentWork.module.scss";
 
 // component
 import SectionTitle from "../molecules/SectionTitle";
+import RecentWorkCard from "../molecules/RecentWorkCard";
 
 const RecentWork = () => {
   return (
     <div className={styles.recent}>
       <div className={styles.container}>
         <SectionTitle key={1} id="#recent" title="Recent My Works" />
+        <div className={styles.cardFlexBox}>
+          <RecentWorkCard
+            key={1}
+            id="#card1"
+            title="QR Gate System"
+            img="/images/gate.jpg"
+            link="/works/qr_gate"
+            text="Automated email based on information entered from Google Forms and spreadsheets, the email is decorated using MJML and includes a numbered ticket number and QR code that will be needed at the entrance of the event. Automation was performed."
+          />
+        </div>
       </div>
     </div>
   );
