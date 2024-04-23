2c3a5ad8040d277e4f68641f660b6a2428d514da
k_95.21 <kohta0421kochi@gmail.com>
2024-04-23 22:08:41 +0900

    chage toppage and sidebar layouts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,16 +5,12 @@ import React from "react";
 import styles from "../styles/page.module.scss";
 
 // components import
-import Top from "@/components/organisms/Top";
 import Header from "@/components/organisms/Header";
-import News from "@/components/organisms/News";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
-      <Top />
-      <News />
     </main>
   );
 }
