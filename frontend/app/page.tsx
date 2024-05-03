b4458923b78fafdf39cbc1cecc593e8a2731cd0e
k_95.21 <kohta0421kochi@gmail.com>
2024-05-03 19:03:43 +0900

    delte all files

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,10 @@
-"use client";
 import React from "react";
 
 // scss
 import styles from "../styles/page.module.scss";
 
 // components import
-import Header from "@/components/organisms/Header";
-import Top from "@/components/organisms/Top";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Header />
-      <Top />
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
