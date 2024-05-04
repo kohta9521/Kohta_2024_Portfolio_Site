3c18fcfd004babc38c11017e2c361dce5966bc19
k_95.21 <kohta0421kochi@gmail.com>
2024-05-04 17:58:26 +0900

    add new top scss

--- frontend/components/atoms/ContactBtn.tsx
+++ frontend/components/atoms/ContactBtn.tsx
@@ -3,6 +3,9 @@ import React from "react";
 // next
 import Link from "next/link";
 
+// hooks
+import useAOS from "@/hooks/useAOS";
+
 // scss
 import styles from "./styles/ContactBtn.module.scss";
 
@@ -10,8 +13,9 @@ import styles from "./styles/ContactBtn.module.scss";
 import { MdContactSupport } from "react-icons/md";
 
 const ContactBtn = () => {
+  useAOS();
   return (
-    <Link className={styles.btn} href="/contact">
+    <Link className={styles.btn} href="/contact" data-aos="fade-up">
       <MdContactSupport className={styles.icon} />
       <p className={styles.text}>話を聞いてみる</p>
     </Link>
