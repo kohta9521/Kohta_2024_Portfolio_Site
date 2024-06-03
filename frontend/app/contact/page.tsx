173eaf2d451245ba949c05eed70a318e8e30fabe
k_95.21 <kohta0421kochi@gmail.com>
2024-06-03 21:56:07 +0900

    add new contct form

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -5,6 +5,9 @@ import styles from "./Contact.module.scss";
 
 // components
 import SubPageLayout from "@/components/templates/SubPageLayout";
+import SubSecTitle from "@/components/molecules/SubSecTitle";
+import BoxTitle from "@/components/molecules/BoxTitle";
+import SubMainText from "@/components/molecules/SubMainText";
 
 const Contact = () => {
   return (
@@ -15,18 +18,36 @@ const Contact = () => {
       subtitle="お問い合わせ"
       desc="私に関するお問い合わせ・Web制作のご依頼はこちらからお問い合わせください。"
     >
-      <h1>Contact us</h1>
+      <SubSecTitle id={1} stringid="summary" text="Contact me" color="green" />
+      <BoxTitle id={1} jaTitle="お問い合わせフォーム" enTitle="Contact Form" />
+      <SubMainText
+        id={2}
+        text="こちらからお問い合わせいただけます。お仕事についてのご相談、見積もりを出して欲しいなどお気軽にご連絡ください。また、お問い合わせいただいたメールアドレスに登録完了メールが届きますので、ご確認ください。お問い合わせいただいた内容については、原則として３営業日以内に返信をいたします。"
+      />
       <form
+        className={styles.form}
         action="https://kohta-engineer-portfolio.form.newt.so/v1/RqLXd0DCc"
         method="post"
       >
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" />
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" />
-        <label htmlFor="message">Message</label>
-        <textarea id="message" name="message"></textarea>
-        <button type="submit">Submit</button>
+        <label className={styles.label} htmlFor="name">
+          ○ Name
+        </label>
+        <input className={styles.input} id="name" name="name" />
+        <label className={styles.label} htmlFor="email">
+          ○ Email
+        </label>
+        <input className={styles.input} id="email" name="email" type="email" />
+        <label className={styles.label} htmlFor="message">
+          ○ Message
+        </label>
+        <textarea
+          className={styles.textarea}
+          id="message"
+          name="message"
+        ></textarea>
+        <button className={styles.button} type="submit">
+          Submit
+        </button>
       </form>
     </SubPageLayout>
   );
